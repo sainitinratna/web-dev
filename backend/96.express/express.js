14f1175b3af51b257fc74eed57e78e42d3334db6
sainitinratna <sainitinratna@gmail.com>
2025-07-17 03:38:13 -0400

    this version includes the logger

--- backend/96.express/express.js
+++ backend/96.express/express.js
@@ -1,4 +1,10 @@
+import logger from "./logger.js";
+import morgan from "morgan";
+
 import express from 'express'//we can use this only when we have module in package.json in type
+
+const morganFormat = ":method :url :status :response-time ms";
+
 
 const app = express()//to create express server
 const port = 3001
@@ -8,6 +14,23 @@ app.use(express.json()) //used to parse a reqques json
 
 let teaData = []//to store data
 let nextId = 1// to init index and store in teadata
+
+//code for logger
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 //updates the teadata with data given by user,to perform this go postman and past url and go to body and then raw after that type a json
 app.post('/tea',(req,res)=>{
